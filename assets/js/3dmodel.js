6706901f4d7554fc3a0c540975d9a887cd18e206
azharanas173 <azharanas173@gmail.com>
2023-12-06 17:57:24 +0700

    feat: add js for 3d model loader

--- assets/js/3dmodel.js
+++ assets/js/3dmodel.js
@@ -6,6 +6,7 @@ const loader = new GLTFLoader();
 
 const canvas = document.getElementById('3dCanvas');
 const renderer = new THREE.WebGLRenderer({ canvas });
+renderer.setSize(window.innerWidth, window.innerHeight);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
@@ -16,9 +17,15 @@ const material = new THREE.MeshBasicMaterial({ color: 0xffff00 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
+directionalLight.position.set(1, 1, 1).normalize();
+scene.add(directionalLight);
 
-const modelUrl = 'MaleCharBaseMesh.gltf';
+camera.position.z = 3;
+camera.position.y = 1;
+camera.position.x = 2;
+
+const modelUrl = 'assets/models/MaleCharBaseMesh.gltf';
 
 loader.load(modelUrl, (gltf) => {
     scene.add(gltf.scene);
